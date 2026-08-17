b8a19e5743590b228fc67f92ae0447f16dc9e43a
Jimi.P <Perkiojimi@gmail.com>
2026-08-17 13:24:45 +0300

    Korjattu erien pisteiden JSON-tallennus: tuple vaihdettu Era-luokkaan

--- Mailapelistatistiikka/Ottelu.cs
+++ Mailapelistatistiikka/Ottelu.cs
@@ -4,24 +4,32 @@ using System.Linq;
 
 namespace Mailapelistatistiikka
 {
+    public class Era
+    {
+        public int Oma { get; set; }
+        public int Vastustaja { get; set; }
+    }
+
     public class Ottelu
     {
         public string Laji { get; set; }
         public string Vastustaja { get; set; }
         public DateTime Paivamaara { get; set; }
         public string Muistiinpanot { get; set; }
-        public List<(int Oma, int Vastustaja)> Erat { get; set; } = new List<(int, int)>();
+        public List<Era> Erat { get; set; } = new List<Era>();
 
         public void LisaaEra(int oma, int vastustaja)
         {
-            Erat.Add((oma, vastustaja));
+            Erat.Add(new Era { Oma = oma, Vastustaja = vastustaja });
         }
 
         public bool OnVoitto()
         {
-            int voitot = Erat.Count(e => e.Oma > e.Vastustaja);  
+            int voitot = Erat.Count(e => e.Oma > e.Vastustaja);
             int haviot = Erat.Count(e => e.Oma < e.Vastustaja);
             return voitot > haviot;
         }
     }
 }
+
+    
